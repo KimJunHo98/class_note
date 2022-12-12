15c0e9aee1c19f3450ba6bad4c29f33f250df31c
KimJunHo98 <dydgnscc6845@gmail.com>
2022-12-12 14:14:10 +0900

    class note

---
+++ js/custom.js
@@ -0,0 +1,82 @@
+//------------------------ html ------------------------
+$("#hbtn1").click(function(e){
+    $(".hday1").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn2").click(function(e){
+    $(".hday2").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn3").click(function(e){
+    $(".hday3").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn4").click(function(e){
+    $(".hday4").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn5").click(function(e){
+    $(".hday5").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn6").click(function(e){
+    $(".hday6").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn7").click(function(e){
+    $(".hday7").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn8").click(function(e){
+    $(".hday8").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn9").click(function(e){
+    $(".hday9").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#hbtn10").click(function(e){
+    $(".hday10").slideToggle(200);
+    $(this).toggleClass("open");
+});
+
+//------------------------ css ------------------------
+$("#cbtn1").click(function(e){
+    $(".cday1").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn2").click(function(e){
+    $(".cday2").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn3").click(function(e){
+    $(".cday3").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn4").click(function(e){
+    $(".cday4").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn5").click(function(e){
+    $(".cday5").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn6").click(function(e){
+    $(".cday6").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn7").click(function(e){
+    $(".cday7").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn8").click(function(e){
+    $(".cday8").slideToggle(200);
+    $(this).toggleClass("open");
+});
+$("#cbtn9").click(function(e){
+    $(".cday9").slideToggle(200);
+    $(this).toggleClass("open");
+});
+
+
+
