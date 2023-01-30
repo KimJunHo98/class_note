df7e54fa14232823308bae5c6e0c4c5defe41262
KimJunHo98 <dydgnscc6845@gmail.com>
2023-01-30 18:26:58 +0900

    tab메뉴 자바스크립트로 바꾸기

--- javascript/09/js/tab-js.js
+++ javascript/09/js/tab-js.js
@@ -1,18 +1,28 @@
-const btn = document.querySelectorAll("ul li"),
-    para = document.querySelectorAll("div h3");
+const btn = document.querySelectorAll('ul>li'), 
+    panel = document.querySelectorAll('h3');
 
-    /*
-        1. 반복되는 기능을 함수로 묶고 변하는 부분을 매개변수 처리
-        2. 반복되는 요소를 변수로 저장
-        3. 1번과 2번에서 작성한 함수와 변수를 사용하여 코드를 재작성하기
-    */
-for(i=0; i<para.length; i++){
 
+// panel.forEach((el)=>{
+//     el.style.display = "none";
+//     panel[0].style.display = "block";
+// });
+hidden(panel);
+visible(panel[0]);
+
+btn.forEach(function(el, i){
+    el.addEventListener("click", function(){
+        hidden(panel);
+        visible(panel[i]);
+    })
+})
+
+function visible(obj) {
+    obj.length == undefined ?  obj.style.display = "block" : obj.forEach((el)=>{
+        el.style.display = "block";
+    });
 }
-// btn.forEach(function(i){
-//     for(i=0; i<btn.length; i++){
-//         btn[i].addEventListener("click", function(){
-            
-//         })
-//     }
-// })
+function hidden(obj) {
+    obj.length == undefined ?  obj.style.display = "none" : obj.forEach((el)=>{
+        el.style.display = "none";
+    });
+}
